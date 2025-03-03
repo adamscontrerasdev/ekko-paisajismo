b45c919aba2b752f8f405d3b34b03468e17569c6
Adams contreras <adamscontreras18@gmail.com>
2025-03-03 14:55:45 -0300

    Fix contact section

--- src/app/screens/Contacto/Contacto.tsx
+++ src/app/screens/Contacto/Contacto.tsx
@@ -1,15 +1,16 @@
-"use client"
-import { useIsMobile } from "@/app/Elements/Hooks/ScreenSizeContext";
-import Link from "next/link";
-import React from "react";
+"use client"; // Usar en cliente
+import React, { useEffect, useState } from "react";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import Link from "next/link";
+import Image from "next/image";
+import { useIsMobile } from "@/app/Elements/Hooks/ScreenSizeContext";
 
 export const Contacto = () => {
-  // Detect if the user is on mobile
+  const [focus, setFocus] = useState(false);
+  const fatherRef = React.useRef<HTMLDivElement>(null);
   const { isMobile } = useIsMobile();
 
-  // Create a mapping of icon names to their components
   const icons = [
     {
       component: FaInstagram,
@@ -26,11 +27,25 @@ export const Contacto = () => {
     {
       component: FaWhatsapp,
       color: "#25D366",
-      root: "https://wa.me/+5491165961777?text=Hola%20Loida%20me%20comunico%20desde%20tu%20p%C3%A1gina%20web%20porque%20me%20interesa%20tus%20servicios%20y%20quisiera%20saber%20cuales%20son%20los%20pasos%20que%20debo%20seguir%20para%20pautar%20una%20cita",
+      root: "https://wa.me/+5491165961777?text=Hola%20Loida...",
     },
   ];
 
-  // Pass the actual component instead of a string
+  useEffect(() => {
+    const handleScroll = () => {
+      const top = fatherRef.current?.getBoundingClientRect().top;
+      if (top !== undefined && top <= 200) {
+        setFocus(true);
+        // Solo activa el rebote una vez
+      } else {
+        setFocus(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []); // Dependencia actualizada
+
   const renderIcon = (
     IconComponent: React.ElementType,
     color: string,
@@ -40,32 +55,74 @@ export const Contacto = () => {
     return (
       <Link
         href={root}
-        className="p-2 flex justify-center items-center rounded-2xl"
+        className={`p-2 flex justify-center items-center rounded-2xl md:hover:scale-110 transition-all duration-300 ${
+          focus ? "animate-uniqueBounce" : "animate-none"
+        }`}
         key={index}
-        target="_blank" // Open in new tab for desktop and WhatsApp
+        target="_blank"
         rel="noopener noreferrer"
         style={{
           background: "linear-gradient(45deg, rgba(0, 0, 0, 0.8), rgba(60, 60, 60, 0.9))",
         }}
       >
-        <IconComponent className="text-4xl" style={{ color }} />
+        <IconComponent
+          className={`text-4xl transition-transform duration-300 ${focus ? "animate-uniqueBounceIcons" : "animate-none"}`}
+          style={{
+            color,
+          }}
+        />
       </Link>
     );
   };
 
   return (
     <div
-      className="w-screen h-screen bg-black pt-28 flex flex-col justify-start relative items-center"
+      className="w-screen h-screen pt-28 flex flex-col justify-start relative items-center overflow-hidden"
       id="contacto"
+      ref={fatherRef}
     >
-      <h1>Contacto</h1>
+      <h1 className="relative z-20">Contacto</h1>
+      <div className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center p-5">
+        <h2
+          className="text-center z-20 relative font-bold"
+          style={{
+            filter: focus ? "blur(0px)" : "blur(10px)",
+            opacity: focus ? 1 : 0,
+            transition: "all 1s ease-in-out",
+          }}
+        >
+          ¿Quieres un espacio para conectar con la naturaleza?
+        </h2>
+      </div>
       <div
-        className="w-80 p-2 bg-[#1c1c1c] bottom-0 absolute flex justify-evenly items-center"
+        className={`w-80 p-2 bg-[#1c1c1c] absolute flex justify-evenly items-center z-20 ${
+          focus ? "animate-uniqueBounceContent" : "animate-none"
+        }`}
         style={{
           borderRadius: "30px 30px 0px 0px",
+          bottom: focus ? "0" : "-100%",
+          transition: "all 0.5s ease-in-out",
         }}
       >
         {icons.map((icon, index) => renderIcon(icon.component, icon.color, index, icon.root))}
+      </div>
+
+      <div className="h-full w-full flex flex-col justify-center items-center absolute top-0 left-0 -z-10">
+        <div
+          className="absolute top-0 left-0 w-full h-full"
+          style={{
+            background: "linear-gradient(to bottom, #000, transparent )",
+            backdropFilter: focus ? "blur(10px)" : "blur(0px)",
+            transition: "all 0.2s ease-out",
+          }}
+        ></div>
+        <Image
+          src="/Img/finalPic.jpg"
+          alt=""
+          width="1920"
+          height="1080"
+          className="w-full h-full object-cover object-center"
+        />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,68 @@ export default {
         primary: "var(--primary)",
         secondary: "var(--secondary)",
       },
+      animation: {
+        uniqueBounce: "uniqueBounce 1s ease-in-out forwards",
+        uniqueBounceIcons: "uniqueBounceIcons 1s ease-in-out forwards 500ms",
+        uniqueBounceContent: "uniqueBounceContent 1s ease-in-out forwards 500ms",
+      },
+      keyframes: {
+        uniqueBounceContent: {
+          "0%": {
+            transform: "scaleY(1.2)",
+            transformOrigin: "bottom",
+            WebkitTransform: "scaleY(1.2)",          
+            WebkitTransformOrigin: "bottom",       
+          },
+          "30%": {
+            transform: "scaleY(0.9)",
+            transformOrigin: "bottom",
+            WebkitTransform: "scaleY(0.9)",        
+            WebkitTransformOrigin: "bottom",       
+          },
+          "50%": {
+            transform: "scaleY(1)",
+            transformOrigin: "bottom",
+            WebkitTransform: "scaleY(1)",          
+            WebkitTransformOrigin: "bottom",       
+          }
+        },
+        
+        uniqueBounce: {
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "30%": {
+            transform: "translateY(-10px)",  
+          },
+          "50%": {
+            transform: "translateY(0)",  
+          },
+          "70%": {
+            transform: "translateY(-4px)",  
+          },
+          "100%": {
+            transform: "translateY(0)",  
+          },
+        },
+        uniqueBounceIcons: {
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "20%": {
+            transform: "translateY(-5px)",
+          },
+          "50%": {
+            transform: "translateY(0)",  
+          },
+          "70%": {
+            transform: "translateY(-2px)", 
+          },
+          "100%": {
+            transform: "translateY(0)",  
+          },
+        },
+      },
     },
   },
   plugins: [],
